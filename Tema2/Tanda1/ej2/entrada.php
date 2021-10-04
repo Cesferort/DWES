6e4c1f648606ba4e99cd4b452e3253ded084dd76
César Ferreiro Ortiz de Guinea <cesferort99@gmail.com>
2021-10-04 14:26:58 +0200

    [UD2][T1] EJ2 - Progreso
    -EJ2 Progreso

---
+++ Tema2/Tanda1/ej2/entrada.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Si eres SOCIO, introduce tu usuario y password</p>
+    <form action="./lib/autenticacion.php" method="post">
+        <table>
+            <tr>
+                <td>USUARIO:</td>
+                <td>
+                    <input type="text" name="nomUser" id="nomUser">
+                </td>
+            </tr>
+            <tr>
+                <td>PASSWORD:</td>
+                <td>
+                    <input type="password" name="passUser" id="passUser">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name="login" value="Acceso Socio">
+                </td>
+            </tr>
+        </table>
+        <hr>
+        <p>Si no dispones de usuario, entra como invitado</p>
+        <input type="submit" name="loginGuest" value="Acceso Invitado">
+    </form>
+</body>
+</html>
