7d91e89e999ec735bbf5099901909feaa4f8f2d0
Cesferort <cesferort99@gmail.com>
2021-10-11 03:33:59 +0200

    [UD2][T2] EJ1 - Terminado
    -EJ1 Terminado. Falta comentar el código.

--- Tema2/Tanda2/elegir.php
+++ Tema2/Tanda2/elegir.php
@@ -1,30 +1,70 @@
 <?php
+session_start();
+
 if(isset($_POST["enviarTipos"]))
 {
     $contTipos=0;
     $hayRepetidos=false;
     $tipoCantidad=[];
-    for($iPregunta=1;$iPregunta<=8&&$hayRepetidos==false;$iPregunta++)
+    $tipoGrabar=[];
+    $tipoGrabar['OTRAS']=false;
+    $listaDatosRepoblar=[];
+    $listaDatosRepoblar[]=[];
+    for($iPregunta=1;$iPregunta<=8;$iPregunta++)
     {
-        $tiposElegidos=[];
+        $inputTipo="inputTipo_".$iPregunta;
+        $selectCantidad="selectCantidad_".$iPregunta;        
         $checkGrabar="checkGrabar_".$iPregunta;
-        if(isset($_POST[$checkGrabar]))
+        $tipo=strtoupper($_POST[$inputTipo]);
+        if($tipo != '')
         {
-            $inputTipo="inputTipo_".$iPregunta;
-            $tipo=$_POST[$inputTipo];
-            if(strtoupper($tipo)=="OTRAS" || !in_array($tipo, $tiposElegidos))
+            if($tipo=="OTRAS" || !array_key_exists($tipo, $tipoGrabar))
             {
-                $selectCantidad="selectCantidad_".$iPregunta;
                 $cantidad=intval($_POST[$selectCantidad]);
-
-                $tiposElegidos[]=$tipo;
                 $tipoCantidad[$tipo]=$cantidad;
+    
+                if(isset($_POST[$checkGrabar]))
+                    $tipoGrabar[$tipo]=true;
+                else
+                    $tipoGrabar[$tipo]=false;
+    
                 $contTipos++;
             }
             else
                 $hayRepetidos=true;
         }
+        $datos=[];
+        $datos[]=$_POST[$inputTipo];
+        $datos[]=$_POST[$selectCantidad];
+        if(isset($_POST[$checkGrabar]))
+            $datos[]=true;
+        else
+            $datos[]=false;
+        $listaDatosRepoblar[]=$datos;
     }
+    $_SESSION["listaDatosRepoblar"]=$listaDatosRepoblar;
+    if($contTipos>=3 && $hayRepetidos==false)
+    {
+        $_SESSION["tipoCantidad"]=$tipoCantidad;
+        $_SESSION["tipoGrabar"]=$tipoGrabar;
+        header('Location: ./lib/jugar?primeraCarga=true.php');
+        die();
+    }
+}
+else
+{
+    $_SESSION["listaDatosRepoblar"]=
+    [
+        [],
+        ['', 1, false],
+        ['', 1, false],
+        ['', 1, false],
+        ['', 1, false],
+        ['', 1, false],
+        ['', 1, false],
+        ['', 1, false],
+        ['', 1, false]
+    ];
 }
 ?>
 <!DOCTYPE html>
@@ -49,6 +89,15 @@ input[type="submit"]
 }
 </style>
 <body>
+    <?php
+    if(isset($_POST["enviarTipos"]))
+    {
+        if($hayRepetidos==true)
+            echo "<h2>No se puede repetir el mismo tipo de pregunta en casillas diferentes</h2>";
+        else if($contTipos<3)
+            echo "<h2>Debe haber al menos 3 tipos distintos</h2>";
+    }
+    ?>
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
         <h2>Configura un mínimo de 3 tipos de preguntas</h2>
         <table>
@@ -59,16 +108,30 @@ input[type="submit"]
                 <th><h2>GRABAR FALLOS</h2></th>
             </tr>
             <?php
+            $listaDatosRepoblar=$_SESSION["listaDatosRepoblar"];
             for($iPregunta=1;$iPregunta<=8;$iPregunta++)
             {
+                $datos=$listaDatosRepoblar[$iPregunta];
+                $tipo=$datos[0];
+                $cantidad=$datos[1];
+                $grabar=$datos[2];
+                
                 $txt="<tr>";
                 $txt.="<td>$iPregunta</td>";
-                $txt.="<td><input type='text' name='inputTipo_".$iPregunta."'></td>";
+                $txt.="<td><input type='text' value='$tipo' name='inputTipo_".$iPregunta."'></td>";
                 $txt.="<td><select name='selectCantidad_".$iPregunta."'>";
                 for($nOption=1;$nOption<=10;$nOption++)
-                    $txt.="<option>$nOption</option>";
+                {
+                    if($nOption==$cantidad)
+                        $txt.="<option selected>$nOption</option>";
+                    else
+                        $txt.="<option>$nOption</option>";
+                }
                 $txt.="</select></td>";
-                $txt.="<td><input type='checkbox' name='checkGrabar_".$iPregunta."'>Grabar fallos</td>";
+                if($grabar==true)
+                    $txt.="<td><input checked type='checkbox' name='checkGrabar_".$iPregunta."'>Grabar fallos</td>";
+                else
+                    $txt.="<td><input type='checkbox' name='checkGrabar_".$iPregunta."'>Grabar fallos</td>";
                 $txt.="</tr>";
                 echo $txt;
             }
--- Tema2/Tanda2/lib/jugar.php
+++ Tema2/Tanda2/lib/jugar.php
@@ -1,3 +1,259 @@
+<?php
+session_start();
+if(!isset($_SESSION["tipoCantidad"]) || !isset($_SESSION["tipoGrabar"]))
+{
+    header('Location: ../elegir.php?errChooseType=true');
+    die();
+}
+
+define("DIR", "../files/fallos_grabados.txt");
+$juegoTerminado=true;
+$_SESSION["preguntas"]=
+[
+    'GEOGRAFIA' => 
+    [
+        [
+            '¿Pregunta1?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta2?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta3?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ]
+    ],
+    'CINE' => 
+    [
+        [
+            '¿Pregunta1?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta2?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta3?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta4?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ]
+    ],
+    'CIENCIAS' => 
+    [
+        [
+            '¿Pregunta1?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta2?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta3?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta4?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta5?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ]
+    ],
+    'OTRAS' => 
+    [
+        [
+            '¿Pregunta1?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta2?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta3?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta4?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta5?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ],
+        [
+            '¿Pregunta6?',
+            'Respuesta Correcta',
+            'Respuesta Erronea1',
+            'Respuesta Erronea2'
+        ]
+    ]
+];
+
+if(isset($_GET["primeraCarga"]))
+{
+    if(file_exists(DIR))
+        unlink(DIR);
+    $_SESSION["cuestionario"]=
+    [
+        'GEOGRAFIA' => [],
+        'CINE' => [],
+        'CIENCIAS' => [],
+        'OTRAS' => []
+    ];
+
+    $tipoCantidad=$_SESSION["tipoCantidad"];
+
+    $tipoCantidad_Limpio=
+    [
+        "GEOGRAFIA" => 0,
+        "CINE" => 0,
+        "CIENCIAS" => 0,
+        "OTRAS" => 0
+    ];
+    foreach($tipoCantidad as $tipo => $cantidad)
+    {
+        if(!array_key_exists($tipo, $tipoCantidad_Limpio))
+            $tipo="OTRAS";
+        $tipoCantidad_Limpio[$tipo] += intval($cantidad);
+    }
+    
+    $preguntas=$_SESSION["preguntas"];
+
+    $cantidadGeografia=$tipoCantidad_Limpio["GEOGRAFIA"];
+    if($cantidadGeografia>0)
+    {   
+        shuffle($preguntas['GEOGRAFIA']);
+        $shuffleGeografia=$preguntas['GEOGRAFIA'];
+        for($i=0;$i<count($shuffleGeografia) && $i<$cantidadGeografia;$i++)
+        {
+            $preguntasFinales=$_SESSION["cuestionario"]['GEOGRAFIA'];
+            $preguntasFinales[]=$shuffleGeografia[$i];
+            $_SESSION["cuestionario"]['GEOGRAFIA']=$preguntasFinales;
+        }  
+    }
+
+    $cantidadCine=$tipoCantidad_Limpio["CINE"];
+    if($cantidadCine>0)
+    {   
+        shuffle($preguntas['CINE']);
+        $shuffleCine=$preguntas['CINE'];
+        for($i=0;$i<count($shuffleCine) && $i<$cantidadCine;$i++)
+        {
+            $preguntasFinales=$_SESSION["cuestionario"]['CINE'];
+            $preguntasFinales[]=$shuffleCine[$i];
+            $_SESSION["cuestionario"]['CINE']=$preguntasFinales;
+        }  
+    }
+
+    $cantidadCiencias=$tipoCantidad_Limpio["CIENCIAS"];
+    if($cantidadCiencias>0)
+    {   
+        shuffle($preguntas['CIENCIAS']);
+        
+        $shuffleCiencias=$preguntas['CIENCIAS'];
+        for($i=0;$i<count($shuffleCiencias) && $i<$cantidadCiencias;$i++)
+        {
+            $preguntasFinales=$_SESSION["cuestionario"]['CIENCIAS'];
+            $preguntasFinales[]=$shuffleCiencias[$i];
+            $_SESSION["cuestionario"]['CIENCIAS']=$preguntasFinales;
+        }  
+    }
+
+    $cantidadOtras=$tipoCantidad_Limpio["OTRAS"];
+    if($cantidadOtras>0)
+    {   
+        shuffle($preguntas['OTRAS']);
+        $shuffleOtras=$preguntas['OTRAS'];
+        for($i=0;$i<count($shuffleOtras) && $i<$cantidadOtras;$i++)
+        {
+            $preguntasFinales=$_SESSION["cuestionario"]['OTRAS'];
+            $preguntasFinales[]=$shuffleOtras[$i];
+            $_SESSION["cuestionario"]['OTRAS']=$preguntasFinales;
+        }  
+    }
+}
+
+function comprobarRespuesta($tipo)
+{
+    if(!isset($_POST["enviarRespuesta_".$tipo]))
+        return false;
+
+    $pregunta=$_POST["pregunta_".$tipo];
+    if(!isset($_POST["respuesta_".$tipo]))
+        $respuesta="NINGUNA";
+    else
+        $respuesta=$_POST["respuesta_".$tipo];
+
+    $listaPreguntas=$_SESSION["preguntas"][$tipo];
+    for($i=0;$i<count($listaPreguntas);$i++)
+    {
+        $preguntaRespuestas=$listaPreguntas[$i];
+        if($preguntaRespuestas[0]==$pregunta)
+        {
+            $respuestaValida=$preguntaRespuestas[1];
+            if($respuestaValida==$respuesta)
+                return true;
+            else
+            {
+                if($_SESSION["tipoGrabar"][$tipo]==true)
+                {
+                    $f = fopen(DIR, 'a');
+                    fwrite($f, "\nTipo: ".$tipo);
+                    fwrite($f, "\nPregunta: ".$pregunta);
+                    fwrite($f, "\nRespuesta Correcta: ".$respuestaValida);
+                    fwrite($f, "\nRespuesta Ofrecida: ".$respuesta."\n");
+                    fclose($f);
+                }
+                return false;
+            }
+        }
+    }  
+    return false;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +262,90 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jugar</title>
 </head>
+<style>
+td, th
+{
+    border:2px black solid;
+}
+input[type="submit"]
+{
+    background-color: blue;
+    color:white;
+    padding:2px;
+}
+</style>
 <body>
-    
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <table>
+            <?php
+            $txt="<tr>";
+            $txt.="<th>TIPO</th>";
+            $txt.="<th>Nº PREGUNTA</th>";
+            $txt.="<th>PREGUNTA</th>";
+            $txt.="<th>RESPUESTAS</th>";
+            $txt.="<th></th>";
+            $txt.="<th></th>";
+            $txt.="</tr>";
+            foreach($_SESSION["cuestionario"] as $tipo => $preguntasFinales)
+            {
+                if(count($preguntasFinales)>0)
+                {
+                    $txt.="<tr>";
+                    $txt.="<td>$tipo</td>";
+                    $nPregunta=1;
+                    if(isset($_POST["nPregunta_".$tipo]))
+                        $nPregunta=intval($_POST["nPregunta_".$tipo]);
+                    
+                    if(isset($_POST["enviarRespuesta_".$tipo]))
+                        $nPregunta++;
+                    
+                    if($nPregunta > count($preguntasFinales))
+                        $txt.="<td></td><td></td><td></td><td></td>";   
+                    else
+                    {
+                        $txt.="<td>$nPregunta</td>";
+                        $preguntaFinal=$preguntasFinales[$nPregunta-1];
+                        $pregunta=$preguntaFinal[0];
+                        $txt.="<td>$pregunta";
+                        $txt.="<input type='hidden' name='pregunta_".$tipo."' value='$pregunta'>";
+                        $txt.="</td>";
+
+                        $respuestas=[];
+                        $respuestas[]=$preguntaFinal[1];
+                        $respuestas[]=$preguntaFinal[2];
+                        $respuestas[]=$preguntaFinal[3];
+                        shuffle($respuestas);
+                        $txt.="<td>";
+                        foreach($respuestas as $respuesta)
+                            $txt.="<input type='radio' name='respuesta_".$tipo."' value='$respuesta'>$respuesta";
+                        $txt.="</td>";
+                        $txt.="<td><input type='submit' name='enviarRespuesta_".$tipo."' value='ENVIAR RESPUESTA'></td>";
+                        
+                        $juegoTerminado=false;
+                    }
+
+                    $txt.="<input type='hidden' name='nPregunta_".$tipo."' value='$nPregunta'>";
+
+                    $aciertos=0;
+                    if(isset($_POST["aciertos_".$tipo]))
+                        $aciertos=$_POST["aciertos_".$tipo];
+
+                    if(comprobarRespuesta($tipo))
+                        $aciertos++;
+
+                    $txt.="<td>$aciertos aciertos";
+                    $txt.="<input type='hidden' name='aciertos_".$tipo."' value='$aciertos'>";
+                    $txt.="</td>";
+                }
+                $txt.="</tr>";
+            }
+            echo $txt;
+            ?>
+        </table>
+    </form>
+    <?php
+    if($juegoTerminado==true && file_exists(DIR))
+        echo "<a target='_blank' href='".DIR."'>Visualiza archivo de fallos grabados</a>";
+    ?>
 </body>
 </html>
